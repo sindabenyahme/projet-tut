fff7391b776a0ddb157053e063f252a849930f4e
Sarra.Boudrigua <sarasousse7@gmail.com>
2024-03-22 13:23:30 +0100

    sarra commit menu 3

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,31 +3,135 @@
 <div class="cards">
   <v-card
     class="mx-auto click-card"
-
+    hover
   >
-    <v-img
-      height="95px"
-      src="../styles\images\entree.png"
+    <v-img class="image"
 
+      src="../assets/entree.png"
     ></v-img>
-
-    <v-card-text>
+    <v-card-text class="card-text">
       Entrée
     </v-card-text>
 </v-card>
+
+  <v-card
+    class="mx-auto click-card"
+    hover
+  >
+    <v-img class="image"
+
+      src="../assets/plat.png"
+    ></v-img>
+    <v-card-text class="card-text">
+      Plat
+    </v-card-text>
+  </v-card>
+
+  <v-card
+    class="mx-auto click-card"
+    hover
+  >
+    <v-img class="image"
+
+      src="../assets/dessert.png"
+    ></v-img>
+    <v-card-text class="card-text">
+      Dessert
+    </v-card-text>
+  </v-card>
+
+  <v-card
+    class="mx-auto click-card"
+    hover
+  >
+    <v-img class="image"
+           src="../assets/supp.png"
+    ></v-img>
+    <v-card-text class="card-text">
+      Supplément
+    </v-card-text>
+  </v-card>
 </div>
+
+
+
+
+  <div class="Entree">
+    <p>Entrée</p>
+    <div class="cards">
+      <v-card
+        class="mx-auto"
+        max-width="344"
+      >
+        <v-img
+          height="200px"
+          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          cover
+        ></v-img>
+
+        <v-card-title>
+          Top western road trips
+        </v-card-title>
+
+        <v-card-subtitle>
+          1,000 miles of wonder
+        </v-card-subtitle>
+
+        <v-card-actions>
+          <v-btn>
+          <font-awesome-icon icon="fa-solid fa-plus" ></font-awesome-icon>
+          </v-btn>
+
+          <v-spacer></v-spacer>
+
+          <v-btn
+            :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+            @click="show = !show"
+          ></v-btn>
+        </v-card-actions>
+
+        <v-expand-transition>
+          <div v-show="show">
+            <v-divider></v-divider>
+
+            <v-card-text>
+              I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+            </v-card-text>
+          </div>
+        </v-expand-transition>
+      </v-card>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const show = ref(false);
 </script>
 
 <style scoped>
-.click-card:active{
+.cards{
+  display: flex;
+  padding: 31px;
+
+
+}
+
+.click-card:active,
+.click-card:focus {
   background-color: red;
+  color: white;
 }
 .click-card{
   max-width: 160px;
   height: 142px;
+}
+.image{
+  width: 200px;
+  height:98px;
+}
+.card-text{
+  text-align: center;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -13,8 +13,26 @@ import App from './App.vue'
 // Composables
 import { createApp } from 'vue'
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import {
+  faTrash,
+  faPenToSquare,
+  faSearch,
+  faPlus,
+
+} from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faTrash,faPenToSquare,faSearch,faPlus)
+
 const app = createApp(App)
 
 registerPlugins(app)
 
-app.mount('#app')
+app.component('font-awesome-icon', FontAwesomeIcon).mount('#app')
