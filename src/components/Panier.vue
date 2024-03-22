7a20b9202b1d2bff9a4067093a68525554f17d06
sinda <sinda.ben_yahmed@etud.univ-jfc.fr>
2024-03-22 16:37:39 +0100

    sinda_Panier_Modif

--- src/components/Panier.vue
+++ src/components/Panier.vue
@@ -1,90 +1,147 @@
 <template>
-    <div class="panier">
-      <v-card v-for="(plat, index) in panier" :key="index" class="plat-card">
-        <v-img :src="plat.image" height="80px" weight="20px"></v-img>
-        <v-btn icon class="delete-btn" @click="deleteItem(index)">
-          <v-icon>mdi-close</v-icon>
+  <h2> Mon Panier </h2>
+  <div class="panier">
+    <v-card class="plat-card" > 
+      <v-img classe ="image"src="../assets/plat.png"></v-img>
+      <p class="plat-nom">salade</p>
+      <v-btn icon class="delete-btn" @click="deleteItem(index)">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+      <div class="card-content">
+        <v-btn icon class="minus-btn" @click="decrementQuantity">
+          <v-icon>mdi-minus</v-icon>
         </v-btn>
-        <div class="card-content">
-          <v-btn icon class="minus-btn" @click="decrementQuantity(index)">
-            <v-icon>mdi-minus</v-icon>
-          </v-btn>
-          <p>{{ plat.quantity }}</p>
-          <v-btn icon class="plus-btn" @click="incrementQuantity(index)">
-            <v-icon>mdi-plus</v-icon>
-          </v-btn>
-        </div>
-        
-      </v-card>
-    </div>
-  </template>
+        <p>{{ quantity }}</p>
+        <v-btn icon class="plus-btn" @click="incrementQuantity">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </div>
+    </v-card>
+    
   
-  <script setup>
-  import { ref } from 'vue';
+  </div>
+
+  <div class="panier">
+    <v-card class="plat-card"> 
+      <v-img classe ="image"src="../assets/supp.png" ></v-img>
+      <p class="plat-nom">Coca</p>
+      <v-btn icon class="delete-btn" @click="deleteItem">
+        <v-icon>mdi-close</v-icon>
+      </v-btn>
+      <div class="card-content">
+        <v-btn icon class="minus-btn" @click="decrementQuantity">
+          <v-icon>mdi-minus</v-icon>
+        </v-btn>
+        <p>{{ quantity }}</p>
+        <v-btn icon class="plus-btn" @click="incrementQuantity">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </div>
+    </v-card>
+    
+    
+  </div>
+
+  <div class="total">
+    <p><b>Total:</b> <span class="prix">5555 euro</span></p>
+    <button class="submit-btn">Soumettre</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const quantity = ref(1);
+
+const deleteItem = (index) => {
   
-  const panier = ref([
-    { image: 'chemin/vers/image1.jpg', quantity: 1 },
-    { image: 'chemin/vers/image2.jpg', quantity: 1 },
-    { image: 'chemin/vers/image3.jpg', quantity: 1 },
-    { image: 'chemin/vers/image4.jpg', quantity: 1 }
-  ]);
-  
-  const deleteItem = (index) => {
-    panier.value.splice(index, 1);
-  };
-  
-  const decrementQuantity = (index) => {
-    if (panier.value[index].quantity > 1) {
-      panier.value[index].quantity--;
-    }
-  };
-  
-  const incrementQuantity = (index) => {
-    panier.value[index].quantity++;
-  };
-  </script>
-  
-  <style scoped>
-  .panier {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
+};
+
+const decrementQuantity = () => {
+  if (quantity.value > 1) {
+    quantity.value--;
   }
+};
+
+const incrementQuantity = () => {
+  quantity.value++;
+};
+</script>
+
+<style scoped>
+
+.total {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 20px;
+}
+.prix{
+  padding-left:300px;
+ 
+  color:green;
+
+}
+.panier {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0px; 
+}
+
+.plat-card {
+  position: relative;
+  display: flex;
+  align-items: center;
+  padding: 5px; 
+  background-color: #cdc5c5;
+  margin-bottom: 20px;
+  width :600px;
+  height: 150px; 
+}
+.card-content {
+  display: flex;
+  align-items: center;
+  gap: 5px;
+}
+
+.delete-btn {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+}
+
+.minus-btn {
+  margin-right: 20px; 
+}
+
+.plus-btn {
+  margin-left: 10px;
+  margin-right: 20px; 
+}
+
+
+
+.plat-nom {
+  flex-grow: 1;
+  flex-shrink: 1;
+  font-size: 20px;
+  padding-bottom: 30px;
+  margin-right: 5px;
+}
+.submit-btn {
+  background-color: red;
+  color: white; 
+  border: none; 
+  border-radius: 5%; 
+  width: 150px; 
+  height: 50px; 
+  font-size: 15px; 
+  margin-top: 20px; 
   
-  .plat-card {
-    position: relative;
-    display: flex;
-    align-items: center;
-    padding: 50px;
-    background-color: #cdc5c5;
-  }
-  
-  .card-content {
-    display: flex;
-    align-items: center;
-    gap: 5px;
-  }
-  
-  .delete-btn {
-    position: absolute;
-    top: 5px;
-    right: 5px;
-  }
-  
-  .minus-btn {
-    margin-right: 30px;
-  }
-  
-  .plus-btn {
-    margin-left: 20px;
-    margin-right: 30px;
-  }
-  
-  .delete-icon {
-    position: absolute;
-    top: 5px;
-    right: 5px;
-    cursor: pointer;
-  }
-  </style>
-  
+}
+
+
+
+</style>
